25d7409460a113a8f109e23ff78b5a9e35a353df
Adrian Ciucanel <adrian.ciucanel@outlook.com>
2017-06-23 19:49:01 +0300

    Draw sorting networks on presentation page without parallel grouping of comparators

--- src/main/webapp/js/sortingNetworkP5Canvas.js
+++ src/main/webapp/js/sortingNetworkP5Canvas.js
@@ -3,11 +3,44 @@
  */
 var sortingNetworkToRender = null;
 
+var drawSortingNetwork = function(p, sortingNetwork) {
+    var scaleLength = 20 + sortingNetwork.comparators.length * 25 + sortingNetwork.comparators.length * 5;
+    var scaledHeight = 20 + sortingNetwork.numberOfWires * 25 + sortingNetwork.numberOfWires * 5;
+    var canvasLength = scaleLength < 600 ? 600 : scaleLength;
+    var canvasLength = canvasLength > 975 ? 975 : canvasLength;
+    var canvasHeight = scaledHeight < 400 ? 400 : scaledHeight;
+    p.createCanvas(canvasLength, canvasHeight);
+    p.background('#152738');
+    p.stroke(126);
+    p.strokeWeight(5);
+    for (var i = 0; i < sortingNetwork.numberOfWires; i++) {
+        var x1 = 20;
+        var y1 = 10 + (canvasHeight/sortingNetwork.numberOfWires) * i;
+        var x2 = canvasLength - 20;
+        var y2 = y1;
+        p.line(x1, y1, x2, y2);
+    }
+    for (i = 0; i < sortingNetwork.comparators.length; i++) {
+        var x1 = 40 + ((canvasLength - 60)/sortingNetwork.comparators.length) * i;
+        var y1 = 10 + (canvasHeight/sortingNetwork.numberOfWires) * (sortingNetwork.comparators[i].topWireNumber);
+        var x2 = x1;
+        var y2 = 10 + (canvasHeight/sortingNetwork.numberOfWires) * (sortingNetwork.comparators[i].bottomWireNumber);
+        p.line(x1, y1, x2, y2);
+        p.fill('#fae');
+        p.stroke('#fae');
+        p.ellipse(x1, y1, 7, 7);
+        p.ellipse(x2, y2, 7, 7);
+        p.stroke(126);
+    }
+};
+
 var sortingNetworkP5Canvas = function(p) {
 
     p.setup = function () {
-        p.createCanvas(600, 400);
-        p.background('#152738');
-        p.line(12,12,12, 120);
+        if (sortingNetworkToRender === null) {
+            p.noCanvas();
+        } else {
+            drawSortingNetwork(p, sortingNetworkToRender)
+        }
     };
 };
